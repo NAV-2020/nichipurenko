360f81f7dc7d1c8f4ad21947669575b89b7e3b12
Nichipurenko <na202867@gmail.com>
2020-06-18 20:48:00 +0300

    18062020

---
+++ Lesson_7_Nichipurenko_A.V/Lesson_7_Nichipurenko_A.V.py
@@ -0,0 +1,28 @@
+word = 'aplle'
+print(word)
+print(len (word))
+
+word_2 = ''
+print(word_2)
+
+word_3 = 'I\'m Dima'
+print(len(word_3))
+
+multiline = ''' Line
+... 2 Line
+... 3 Line '''
+print(len(multiline))
+
+
+a  = 'a'
+print(a*10)
+
+print (ord (a)) #97
+
+print(chr(97))
+
+example = 'silly walks'
+for i in range(len (example)):
+    print(example[i], end=' ')
+
+
