618cd9dbc50640fadc020f25e02cc5e90a3384c4
Nichipurenko <na202867@gmail.com>
2020-06-24 22:46:35 +0300

    DZ

--- Lesson_7_DZ_Nichipurenko_A.V/Lesson_7_DZ_2_Nichipurenko_A.V.py
+++ Lesson_7_DZ_Nichipurenko_A.V/Lesson_7_DZ_2_Nichipurenko_A.V.py
@@ -8,7 +8,21 @@
 '''
 
 messag_e = input("Введіть текст: ")
-message_1 = input("Введіть слова: ")
-print(messag_e.find(message_1))
+messag_key = input("Введіть слова: ")
 
+#messag_e = 'Возвращает кортеж, содержащий часть перед последним шаблоном, сам шаблон, и часть после шаблона.'
+#messag_key = 'кортеж, часть, сам'
+
+text_2 = ''
+
+
+for text in messag_e.split(): # перебір тексту
+    for text_1 in messag_key.split(', '): # перебір ключевих слів 
+        if text_1 in text:
+            text = text.upper() # верхній регістр
+    text_2 += text + ' ' 
+
+text_2 = text_2.rstrip()
+
+print(text_2)
 
