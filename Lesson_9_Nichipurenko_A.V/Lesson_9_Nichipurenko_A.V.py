ead6a5b7670d6384b2f680f0331486fcda0194ff
Nichipurenko <na202867@gmail.com>
2020-06-25 22:09:27 +0300

    25062020

---
+++ Lesson_9_Nichipurenko_A.V/Lesson_9_Nichipurenko_A.V.py
@@ -0,0 +1,27 @@
+print("Hello word")
+
+words = ["Hello", "words"]
+print(len(words))
+
+s = "hello" + str(6)
+print(s)
+
+nums = [2,1,3,5,8,]
+squares = (n**2 for n in nums)
+q = list(squares)
+print(q)
+
+print(tuple(nums))
+
+
+colors = ['red', 'green']
+colors_ocunts = [('red', 1), ('green', 2)]
+print(colors_ocunts)
+print(len(colors_ocunts))
+
+print(bool(0))
+print(bool(5))
+print(bool(range(0)))
+
+
+
