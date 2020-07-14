63439ba2cc753357ce5f6f5b4aeb8823d188a1bd
Nichipurenko <na202867@gmail.com>
2020-07-14 16:53:14 +0300

    DZ

--- Lesson_12_DZ_Nichipurenko_A.V/Lesson_12_DZ_Nichipurenko_A.V.py
+++ Lesson_12_DZ_Nichipurenko_A.V/Lesson_12_DZ_Nichipurenko_A.V.py
@@ -1,11 +1,10 @@
 
-"""
-Написати функцію-декоратор, яка підносить до квадрату значення, 
-що повертає функція, до якої декоратор застосовується.
-"""
-
 def decorFunc(func):
-    
+    """
+    Написати функцію-декоратор, яка підносить до квадрату значення, 
+    що повертає функція, до якої декоратор застосовується.
+    """
+
     def funcOne(arg1, arg2, arg3):
         varOne = func(arg1, arg2, arg3)
         result = [i**2 for i in varOne]
@@ -16,13 +15,15 @@ def decorFunc(func):
 @decorFunc
 def my_func(val1,val2, val3):
     var_result = [i for i in range (val1, val2, val3)]
-    print("Початковий список значень: ", var_result, '\n')
+    print()
+    print("Cписок значень: ", var_result, '\n')
     return var_result
 
 print("*"*50)
-print("Задайте діапазон чисел для піднесення до квадрату.\n")
+
+print("Задайте діапазон чисел піднесення до квадрату.\n")
 var1 = int(input("Введіть початкове значення :"))
 var2 = int(input("Введіть кінцеве значення :"))
 var3 = int(input("Введіть крок між значеннями :"))
 
-print("Значення піднесені до квадрату: ", my_func(var1, var2, var3))
+print("Значення піднесені до квадрату: ", my_func(var1, var2, var3), '\n')
