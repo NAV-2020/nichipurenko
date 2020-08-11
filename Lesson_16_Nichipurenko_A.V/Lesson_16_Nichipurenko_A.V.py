d54662eebf4a7f7336477d0bc997e68c95c6260a
Nichipurenko <na202867@gmail.com>
2020-08-11 22:17:34 +0300

    11082020

---
+++ Lesson_16_Nichipurenko_A.V/Lesson_16_Nichipurenko_A.V.py
@@ -0,0 +1,23 @@
+
+
+d={
+    1:"Hello",
+    "key_2":"python",
+    345:"!!!"
+}
+
+print(d[1])
+print(len(d))
+
+a = d
+print(a)
+
+s = d.copy()
+print(s)
+
+s.clear()
+print(s)
+
+print(d.keys())
+
+print(d.pop(1))
