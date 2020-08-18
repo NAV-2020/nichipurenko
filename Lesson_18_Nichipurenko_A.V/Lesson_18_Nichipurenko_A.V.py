f3ad15c7b9b02e13c4731f701d7bc93a2dcf45bc
Nichipurenko <na202867@gmail.com>
2020-08-18 23:02:18 +0300

    18082020

---
+++ Lesson_18_Nichipurenko_A.V/Lesson_18_Nichipurenko_A.V.py
@@ -0,0 +1,18 @@
+
+#stream = open(".\f1.txt", mode = 'r', encoding = None)
+#stream = open("f1.txt", 'r')
+#stream = open("f2.txt", 'w')
+#stream = open("f2.txt", 'r')
+
+#stream = open('f2.txt', 'r')
+#stream.close()
+
+"""
+stream = open('f2.txt')
+file_obj = stream.read()
+print(file_obj)
+for el in file_obj:
+    print(el)
+
+"""
+
