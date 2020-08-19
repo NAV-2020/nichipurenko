9b0b49096f246fd6f226c74781c16f7e00ad0c3e
Nichipurenko <na202867@gmail.com>
2020-08-19 22:23:56 +0300

    DZ

---
+++ Lesson_17_DZ_Nichipurenko_A.V/Lessin_17_DZ_3_Nichipurenko_A.V.py
@@ -0,0 +1,41 @@
+"""
+Есть некоторый текст. Посчитайте в этом тексте количество 
+предложений и выведите на экран полученныйрезультат.
+"""
+
+
+def number_of_offers(text: int) -> str:
+    sentence = 0
+    for i in text:
+        if ((i=='.') or (i=='!') or (i=='?')):
+            sentence +=1
+    print(type(sentence))
+    return(sentence)
+
+if __name__ == "__main__":
+
+    TEXT = 'Текст'
+    EXIT = 'Вихід'
+
+    print("""
+        Есть некоторый текст. Посчитайте в этом тексте количество 
+        предложений и выведите на экран полученныйрезультат."""
+    )
+
+    while True:
+        print("""
+        Для виходу введіть -> Вихід
+        Для введення тексту введіть -> Текст
+        """)
+      
+        choice = input('Вибір: ')
+        print()
+
+        if choice == EXIT:
+            break
+        elif choice == TEXT:
+            text = input("Введіть текст: ")
+            print("-" * 40)
+            print('Кількість речень - ', number_of_offers(text))
+            print()
+            input('Натисніть ENTER щоб продовжити...')
